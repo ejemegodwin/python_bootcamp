74d597065aa09c8efff6db50f1c13940774fdab5
Focus the Asylum <godwinejeme@gmail.com>
2026-07-13 21:56:58 +0100

    python3

---
+++ lesson25.py
@@ -0,0 +1,33 @@
+name = "Godwin"
+
+for letter in name:
+    print(letter)
+
+print(len(name))
+
+word = "Python"
+print(word[0])
+print(word[-1])
+print(len(word))
+
+for char in word:
+    print(char)
+
+text = "Go"
+for letter in text:
+    print(letter * 3)
+
+
+mentor question; we use count += 1 so we can count 1 after the other inside of a loop. moreover count = 1 is a variable
+
+
+
+Exercise; word = "Python"
+
+print(word[0])
+
+print(word[-1])
+
+print(len(word))
+
+ challenge; it will print GGG and ooo, because it's been multiply by 3 
